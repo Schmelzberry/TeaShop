a87755c43d17144398dffe21252464506d79d3fd
Jake Elsberry <jelsberry96@gmail.com>
2023-11-19 11:01:51 -0800

    feature new tea form

--- src/components/NewTeaForm.js
+++ src/components/NewTeaForm.js
@@ -1,9 +1,34 @@
 import React from "react";
 
-function NewTeaForm(props){
+function NewTeaForm(){
+
+  function handleNewTeaSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.origin.value);
+    console.log(event.target.price.value);
+    console.log(event.target.type.value);
+  }
   return(
     <React.Fragment>
-      <h3>Form will go here!</h3>
+      <form onSubmit={handleNewTeaSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name of tea' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <input
+          name='type'
+          placeholder='organic or non-organic' />
+        <button type='submit'>Tea Me!</button>
+      </form>
     </React.Fragment>
   );
 }
