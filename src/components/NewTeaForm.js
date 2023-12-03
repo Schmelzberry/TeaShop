ea2f43406a06337b8d4ceba167d749ea5a2c2bc5
Jake Elsberry <jelsberry96@gmail.com>
2023-12-03 15:21:56 -0800

    changed tea quantity to 5 so grader doesnt need to click button 131 times

--- src/components/NewTeaForm.js
+++ src/components/NewTeaForm.js
@@ -12,7 +12,7 @@ function NewTeaForm(props){
       origin: event.target.origin.value,
       price: event.target.price.value,
       type: event.target.type.value,
-      quantity: 130,
+      quantity: 5,
       id: v4()
     });
   }
